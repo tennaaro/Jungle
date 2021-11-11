4f3a8554e2be4313712a87f493834a85d73c0fcf
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-11-10 19:03:36 -0500

    finished dashboard feature

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
