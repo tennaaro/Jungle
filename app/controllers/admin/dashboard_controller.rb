3eaebd2ab6419fdf2ad33ff1d8e8a85865188a6d
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-11-10 20:08:36 -0500

    added feature about us page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV['username'], password: ENV['password']
   def show
     @products_count = Product.count
     @categories_count = Category.count
